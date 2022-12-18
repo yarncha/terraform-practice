8555d178a381d474d639c220668bf4a8f665106a
Yumin <dbals9812@naver.com>
2022-12-18 15:53:25 +0900

    Update vpc.tf

--- practice3_aws_3tier/vpc.tf
+++ practice3_aws_3tier/vpc.tf
@@ -44,6 +44,7 @@ resource "aws_subnet" "subnet_ap_a" {
     Name = "sbn-con-ojt-ap-pri-a"
   }
 }
+
 resource "aws_subnet" "subnet_ap_c" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = "10.10.130.128/26"
@@ -53,6 +54,7 @@ resource "aws_subnet" "subnet_ap_c" {
     Name = "sbn-con-ojt-ap-pri-c"
   }
 }
+
 resource "aws_subnet" "subnet_db_a" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = "10.10.130.192/28"
@@ -62,6 +64,7 @@ resource "aws_subnet" "subnet_db_a" {
     Name = "sbn-con-ojt-db-pri-a"
   }
 }
+
 resource "aws_subnet" "subnet_db_c" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = "10.10.130.208/28"
@@ -71,6 +74,7 @@ resource "aws_subnet" "subnet_db_c" {
     Name = "sbn-con-ojt-db-pri-c"
   }
 }
+
 resource "aws_subnet" "subnet_attach_a" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = "10.10.130.224/28"
@@ -80,6 +84,7 @@ resource "aws_subnet" "subnet_attach_a" {
     Name = "sbn-con-ojt-attach-pri-a"
   }
 }
+
 resource "aws_subnet" "subnet_attach_c" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = "10.10.130.240/28"
@@ -100,66 +105,80 @@ resource "aws_internet_gateway" "vpc_igw" {
 }
 
 ### [Elastic IP] ###
-resource "aws_eip" "nat" {
-#   vpc = true
+resource "aws_eip" "nat_a" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.vpc_igw]
+
   tags = {
-    Name = "eip-con-ojt"
+    Name = "eip-con-ojt-a"
+  }
+}
+
+resource "aws_eip" "nat_b" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.vpc_igw]
+
+  tags = {
+    Name = "eip-con-ojt-b"
   }
 }
 
 ### [Nat Gateway] ###
-resource "aws_nat_gateway" "nat_gw" {
-  allocation_id = aws_eip.nat.id
+resource "aws_nat_gateway" "nat_gw_a" {
+  allocation_id = aws_eip.nat_a.id
   subnet_id     = aws_subnet.subnet_pub_a.id
   depends_on    = [aws_internet_gateway.vpc_igw]
 
   tags = {
-    Name = "nat-con-ojt"
+    Name = "nat-con-ojt_a"
+  }
+}
+
+resource "aws_nat_gateway" "nat_gw_c" {
+  allocation_id = aws_eip.nat_b.id
+  subnet_id     = aws_subnet.subnet_pub_c.id
+  depends_on    = [aws_internet_gateway.vpc_igw]
+
+  tags = {
+    Name = "nat-con-ojt_c"
   }
 }
 
 ### [Route Table] ###
-resource "aws_route_table" "route_table_pub" {
-  vpc_id = aws_vpc.vpc.id
+# vpc의 메인 route table, 생성하지 않을 시 자동 생성됨
+resource "aws_default_route_table" "route_table_pub" {
+  default_route_table_id = aws_vpc.vpc.default_route_table_id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat_gw.id
+    gateway_id = aws_internet_gateway.vpc_igw.id
   }
 
   tags = {
     Name = "rt-con-ojt-pub"
   }
 }
-# resource "aws_route_table" "route_table" {
-#   vpc_id = aws_vpc.vpc.id
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.vpc_igw.id
-#   }
 
-#   tags = {
-#     Name = var.route_table_name
-#   }
-# }
+# 프라이빗은 nat랑 연결
 resource "aws_route_table" "route_table_pri_a" {
   vpc_id = aws_vpc.vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat_gw.id
+    gateway_id = aws_nat_gateway.nat_gw_a.id
   }
 
   tags = {
     Name = "rt-con-ojt-pri-a"
   }
 }
+
 resource "aws_route_table" "route_table_pri_c" {
   vpc_id = aws_vpc.vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat_gw.id
+    gateway_id = aws_nat_gateway.nat_gw_c.id
   }
 
   tags = {
@@ -168,47 +187,42 @@ resource "aws_route_table" "route_table_pri_c" {
 }
 
 ### Association ###
-# resource "aws_main_route_table_association" "mainasso" {
-#   vpc_id         = aws_vpc.vpc.id
-#   route_table_id = aws_route_table.rtmain.id
-# }
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.subnet_pub_a.id
+  route_table_id = aws_default_route_table.route_table_pub.id
+}
 
-# resource "aws_route_table_association" "public-a" {
-#   subnet_id      = aws_subnet.subnet1.id
-#   route_table_id = aws_route_table.route_table.id
-# }
+resource "aws_route_table_association" "public_c" {
+  subnet_id      = aws_subnet.subnet_pub_c.id
+  route_table_id = aws_default_route_table.route_table_pub.id
+}
 
-# resource "aws_route_table_association" "public-c" {
-#   subnet_id      = aws_subnet.subnet2.id
-#   route_table_id = aws_route_table.route_table.id
-# }
+resource "aws_route_table_association" "private_ap_a" {
+  subnet_id      = aws_subnet.subnet_ap_a.id
+  route_table_id = aws_route_table.route_table_pri_a.id
+}
 
-# resource "aws_route_table_association" "private-web-a" {
-#   subnet_id      = aws_subnet.subnet3.id
-#   route_table_id = aws_route_table.rtmain.id
-# }
+resource "aws_route_table_association" "private_ap_c" {
+  subnet_id      = aws_subnet.subnet_ap_c.id
+  route_table_id = aws_route_table.route_table_pri_c.id
+}
 
-# resource "aws_route_table_association" "private-web-c" {
-#   subnet_id      = aws_subnet.subnet4.id
-#   route_table_id = aws_route_table.rtmain.id
-# }
+resource "aws_route_table_association" "private_db_a" {
+  subnet_id      = aws_subnet.subnet_db_a.id
+  route_table_id = aws_route_table.route_table_pri_a.id
+}
 
-# resource "aws_route_table_association" "private-was-a" {
-#   subnet_id      = aws_subnet.subnet5.id
-#   route_table_id = aws_route_table.rtmain.id
-# }
+resource "aws_route_table_association" "private_db_c" {
+  subnet_id      = aws_subnet.subnet_db_c.id
+  route_table_id = aws_route_table.route_table_pri_c.id
+}
 
-# resource "aws_route_table_association" "private-was-c" {
-#   subnet_id      = aws_subnet.subnet6.id
-#   route_table_id = aws_route_table.rtmain.id
-# }
+resource "aws_route_table_association" "private_attach_a" {
+  subnet_id      = aws_subnet.subnet_attach_a.id
+  route_table_id = aws_route_table.route_table_pri_a.id
+}
 
-# resource "aws_route_table_association" "private-db-a" {
-#   subnet_id      = aws_subnet.subnet7.id
-#   route_table_id = aws_route_table.rtmain.id
-# }
-
-# resource "aws_route_table_association" "private-db-c" {
-#   subnet_id      = aws_subnet.subnet8.id
-#   route_table_id = aws_route_table.rtmain.id
-# }
+resource "aws_route_table_association" "private_attach_c" {
+  subnet_id      = aws_subnet.subnet_attach_c.id
+  route_table_id = aws_route_table.route_table_pri_c.id
+}
